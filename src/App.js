f1f0f08f71e739fc4d35138b5f47502923bd0f3c
Julie Johnson <jjgator417@gmail.com>
2019-09-21 16:38:50 -0500

    set data to state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="app">
-      Hello
-    </div>
-  );
+const DATA = require('./carrier_cards.json');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      cards: [],
+      searched: null,
+      searchTime: null
+    };
+  }
+
+  componentDidMount () {
+    // API call would go here
+    let time = Math.ceil(DATA.search_time/60000);
+
+    this.setState({
+      cards: DATA.carrier_cards,
+      searched: DATA.carriers_searched,
+      searchTime: time
+    });
+  }
+
+  render () {
+    let { searched, searchTime } = this.state;
+
+    return (
+      <div className="app">
+        {searched && searchTime &&
+        <span>You just compared {searched} rates in {searchTime} minutes!</span>}
+      </div>
+    );
+  }
 }
 
 export default App;
