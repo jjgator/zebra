e643ec163fd901cf562992a9fd223350d5065684
Julie Johnson <jjgator417@gmail.com>
2019-09-24 22:41:00 -0500

    add open state and transitions

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,46 +6,61 @@ import Rate from '../Rate/Rate';
 import Features from '../Features/Features';
 import Details from '../Details/Details';
 
-const Card = (props) => {
-    let { 
-        cornerTag, name, tagline, stars, rate, features, 
-        tag, type, link, details
-    } = props;
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="card">
-            <div className="header">
-                <Logo 
-                    cornerTag={cornerTag}/>
-                <Carrier
-                    name={name}
-                    tagline={tagline}
-                    stars={stars}
-                    features={features}/>
-                <Rate 
-                    rate={rate}
-                    tag={tag}
-                    type={type}
-                    link={link}/>
+        this.state = {
+            open: false
+        };
+    }
+
+    onArrowClick = () => {
+        this.setState({ open: !this.state.open});
+    };
+
+    render() {
+        let { open } = this.state;
+        let { 
+            cornerTag, name, tagline, stars, rate, features, 
+            tag, type, link, details
+        } = this.props;
+    
+        return (
+            <div className="card">
+                <div className="header">
+                    <Logo 
+                        cornerTag={cornerTag}/>
+                    <Carrier
+                        name={name}
+                        tagline={tagline}
+                        stars={stars}
+                        features={features}/>
+                    <Rate 
+                        rate={rate}
+                        tag={tag}
+                        type={type}
+                        link={link}/>
+                </div>
+    
+                {((features && features[0]) || details) &&
+                <button className="arrow-button" onClick={this.onArrowClick}>
+                    <div className={`arrow ${open ? 'open' : ''}`}></div>
+                </button>}
+    
+                <div className={`main ${open ? 'open' : ''}`}>
+                    <Features
+                        features={features}/>
+                    <Details 
+                        details={details}
+                        name={name}
+                        hasFeatures={
+                            features && features[0] 
+                            ? true : false}/>
+                </div>
             </div>
-
-            {((features && features[0]) || details) &&
-            <button className="arrow-button">
-                <div className="arrow"></div>
-            </button>}
-
-            <div className="main">
-                <Features
-                    features={features}/>
-                <Details 
-                    details={details}
-                    name={name}
-                    hasFeatures={
-                        features && features[0] 
-                        ? true : false}/>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Card;
